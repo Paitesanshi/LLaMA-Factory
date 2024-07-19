55274e265815e719a4f26b8ce2a8eb32166244f3
Paitesanshi <zxcptss@gmail.com>
2024-07-19 06:22:53 +0000

    FEAT: add script

---
+++ api.sh
@@ -0,0 +1,7 @@
+nohup llamafactory-cli api examples/inference/phi.yaml >api/phi.log 2>&1 &
+nohup llamafactory-cli api examples/inference/mistral.yaml >api/mistral.log 2>&1 &
+nohup llamafactory-cli api examples/inference/llama3.yaml >api/llama3.log 2>&1 &
+nohup llamafactory-cli api examples/inference/bc7b.yaml >api/bc_7b.log 2>&1 &
+nohup llamafactory-cli api examples/inference/bc13b.yaml >api/bc13b.log 2>&1 &
+nohup llamafactory-cli api examples/inference/qwen7b.yaml >api/qwen7b.log 2>&1 &
+nohup llamafactory-cli api examples/inference/qwen14b.yaml >api/qwen14b.log 2>&1 &
